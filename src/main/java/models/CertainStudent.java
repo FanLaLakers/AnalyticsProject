f4add4e478b4e62b515c65c68b36501f4499765b
IgorBorodin2004 <bis220204@gmail.com>
2023-12-25 03:04:51 +0500

    add models

--- src/main/java/models/CertainStudent.java
+++ src/main/java/models/CertainStudent.java
@@ -1,8 +1,12 @@
 package models;
 
 public class CertainStudent extends Students{
+    private Courses[] Course_Progress;
     public CertainStudent(String FirstName, String LastName, String NameOfGroup) {
         super(FirstName, LastName, NameOfGroup);
     }
 
+    public Courses[] getCourse_Progress() {
+        return Course_Progress;
+    }
 }
